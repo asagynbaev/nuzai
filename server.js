5e7125128c7005c25cd118bbd958c94ff457b993
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2021-12-02 23:02:27 +0100

    aaaa

--- server.js
+++ server.js
@@ -1,86 +1,28 @@
-const forceSSL = require("express-force-ssl");
-const express = require("express")();
-const https = require("https");
-const http = require("http");
-const next = require("next");
-const path = require("path");
-const fs = require("fs");
+// server.js
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
 
-process.env.DO_LOG = Boolean(process.env.npm_config_do_log);
-process.env.NODE_ENV = process.env.npm_config_dev ? "development" : "production";
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-const dev = process.env.NODE_ENV !== "production";
-const nextjs = next({ dev });
-const nextHandler = nextjs.getRequestHandler();
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
 
-// const HTTPS_PORT = process.env.HTTPS_PORT || 3001;
-const HTTP_PORT = 8080;
-
-async function tryer(name, middleware, req, res, next) {
-    try { await middleware(req, res, next) }
-    catch(e) { console.error(`----> ${name} error`, e, new Error().stack) }
-}
-
-async function handleImage(req, res, next) {
-    try {
-        const parsedUrl = req.baseUrl.split("/").slice(1); // split and remove first empty
-        const parsedPath = parsedUrl.slice(0, -1); // get only path
-        const fileName = parsedUrl.slice(-1)[0]; // get only file name
-
-        async function tryExtension(parsedPath, folderName, fileName, extension) {
-            const relativePath = path.join(...parsedPath, folderName, `${fileName}.${extension}`);
-            
-            try {
-                const dynamicPath = path.join(process.cwd(), relativePath);
-                await fs.promises.access(dynamicPath);
-                return dynamicPath;
-            } catch(e) {}
-
-            try {
-                const staticPath = path.join(process.cwd(), "public", relativePath);
-                await fs.promises.access(staticPath);
-                return staticPath;
-            } catch(e) {}
-        }
-        
-        if(req.headers.accept.includes("image/webp")) {
-            const result = await tryExtension(parsedPath, "webp", fileName, "webp");
-            if(result) return res.sendFile(result);
-        } else {
-            const pngResult = await tryExtension(parsedPath, "png", fileName, "png");
-            if(pngResult) return res.sendFile(pngResult);
-
-            const jpgResult = await tryExtension(parsedPath, "jpg", fileName, "jpg");
-            if(jpgResult) return res.sendFile(jpgResult);
-        }
-
-        throw new Error();
-    } catch(e) { next() }
-}
-
-(async () => {
-    try {
-        await nextjs.prepare();
-
-        // https.createServer({
-        //     cert: fs.readFileSync("SSL/cert.pem"),
-        //     key: fs.readFileSync("SSL/key.pem")
-        // }, express).listen(HTTPS_PORT);
-        http.createServer(express).listen(HTTP_PORT);
-        
-        express.use(async (...args) => await tryer("forceSSL", forceSSL, ...args));
-        
-        express.use("/images/*", async (...args) => await tryer("image handler", handleImage, ...args));
-        
-        express.all("/api/*", async (...args) => await tryer("next api", nextHandler, ...args));
-        express.get("*", async (...args) => await tryer("next main", nextHandler, ...args));
-
-        console.log(`--> Process environment: '${process.env.NODE_ENV}'`);
-        console.log(`--> Is app in development mode: ${dev}`);
-        console.log(`-> Ready on port ${HTTP_PORT} for HTTP`);
-        // console.log(`-> Ready on port ${HTTPS_PORT} for HTTPS`);
-	} catch(e) {
-		console.error(e.stack);
-		process.exit(1);
-	}
-})();
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
