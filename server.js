c27eb8ed4e11aa2e046653cdfcfd4f1ef52ea73e
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2021-12-03 00:44:04 +0100

    sdfsfd

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
 app.prepare().then(() => {
   const server = express()
 
+  
   server.all('*', (req, res) => {
     return handle(req, res)
   })
